76a6f982955552ef1ac3fe46be19f3b225d56708
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-23 22:58:04 +0200

    finish check connectivity app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:check_connectivity/helper/connectivity_controller.dart';
+import 'package:check_connectivity/screens/home_screen.dart';
 import 'package:check_connectivity/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -10,24 +12,33 @@ class CheckConnectivityApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    return ValueListenableBuilder(valueListenable: valueListenable, builder: (context, value, child) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Check Connectivity App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        ),
-        home: value ? NoNetworkScreen() : const HomeScreen(),
-      );
-    });
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Check Connectivity App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: NoNetworkScreen(),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, _) {
+        if (!value) {
+          return const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Check Connectivity App',
+            home: NoNetworkScreen(),
+          );
+        } else {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Check Connectivity App',
+            home: const HomeScreen(),
+            builder: (context, child) {
+              return Scaffold(
+                body: Builder(
+                  builder: (context) {
+                    ConnectivityController.instance.init();
+                    return child!;
+                  },
+                ),
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
