18ae3e8f3e0ce02d6b18a292c95355a7cb3bf329
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-23 22:33:06 +0200

    add connectivity_plus package create connectivity controller class

---
+++ lib/helper/connectivity_controller.dart
@@ -0,0 +1,9 @@
+class ConnectivityController {
+  ConnectivityController._();
+
+  static final ConnectivityController instance = ConnectivityController._();
+
+  void init() {
+      // Initialize connectivity monitoring here
+  }
+}
