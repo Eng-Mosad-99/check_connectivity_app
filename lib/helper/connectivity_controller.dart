506a058a444007f48bb941a4f7ec96d21cc9054d
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-23 22:47:37 +0200

    finish connectivity controller class

--- lib/helper/connectivity_controller.dart
+++ lib/helper/connectivity_controller.dart
@@ -1,9 +1,29 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+
 class ConnectivityController {
   ConnectivityController._();
 
   static final ConnectivityController instance = ConnectivityController._();
+  ValueNotifier<bool> isConnected = ValueNotifier<bool>(true);
 
-  void init() {
-      // Initialize connectivity monitoring here
+  Future<void> init() async {
+    // Initialize connectivity monitoring here
+    final result = await Connectivity().checkConnectivity();
+    isInternetConnected(result);
+    Connectivity().onConnectivityChanged.listen(isInternetConnected);
+  }
+
+  bool isInternetConnected(List<ConnectivityResult> results) {
+    // Implement logic to check internet connectivity
+    if (results.contains(ConnectivityResult.none)) {
+      isConnected.value = false;
+      return false; // No connectivity results available
+    } else if (results.contains(ConnectivityResult.mobile) ||
+        results.contains(ConnectivityResult.wifi)) {
+      isConnected.value = true;
+      return true; // Internet is connected
+    }
+    return false; // No internet connection
   }
 }
