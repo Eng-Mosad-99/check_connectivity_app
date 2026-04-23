bae7d4d21c5bf4e1cd99f2249dc9cf5158de0825
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-23 22:29:20 +0200

    create no network screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CheckConnectivityApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CheckConnectivityApp extends StatelessWidget {
+  const CheckConnectivityApp({super.key});
 
   // This widget is the root of your application.
   @override
---
+++ lib/screens/no_network_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NoNetworkScreen extends StatelessWidget {
+  const NoNetworkScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/no_network.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+  }
+}
